dced3298c50d667e53aa49ce0b580642dcf5da0e
Lingraj Mahanand <lmahanand@gmail.com>
2021-12-23 07:58:22 +0530

    deploy script changes

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -7,20 +7,20 @@ async function main() {
     let bal = await waffle.provider.getBalance(deployer.address);
     console.log("ETH Balance of Manager: ", bal);
 
-    // const CompoundRegistry = await ethers.getContractFactory("CompoundRegistry");
-    // compoundRegistry = await CompoundRegistry.deploy();
-    // await compoundRegistry.deployed();
-    // console.log("CompoundRegistry deployed to:", compoundRegistry.address);
+    const CompoundRegistry = await ethers.getContractFactory("CompoundRegistry");
+    compoundRegistry = await CompoundRegistry.deploy();
+    await compoundRegistry.deployed();
+    console.log("CompoundRegistry deployed to:", compoundRegistry.address);
 
     const Compound = await ethers.getContractFactory("Compound");
-    const compound = await Compound.deploy('0x6F48C09d171F1526Bf88fA718bbe87e307e03EaF');
+    const compound = await Compound.deploy(compoundRegistry.address);
     await compound.deployed();
     console.log("Compound deployed to:", compound.address);
 
-    // const LFGlobalEscrow = await ethers.getContractFactory("LFGlobalEscrow");
-    // const escrow = await LFGlobalEscrow.deploy(compound.address);
-    // await escrow.deployed();
-    // console.log("LFGlobalEscrow deployed to:", escrow.address);
+    const LFGlobalEscrow = await ethers.getContractFactory("LFGlobalEscrow");
+    const escrow = await LFGlobalEscrow.deploy(compound.address);
+    await escrow.deployed();
+    console.log("LFGlobalEscrow deployed to:", escrow.address);
 }
 
 main()
